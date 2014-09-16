be609f23fd8c5f0f42cb751c6a435ee13050df47
David Tongeman <Tongeman@users.noreply.github.com>
2014-09-16 15:25:28 +0000

    frmeditbooking

---
+++ RedboxAddin/Presentation/frmEditBooking.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace RedboxAddin.Presentation
+{
+    partial class frmEditBooking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEditBooking));
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label76 = new System.Windows.Forms.Label();
+            this.txtCharge = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTeacher = new System.Windows.Forms.Label();
+            this.lblSchool = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescription.Location = new System.Drawing.Point(165, 146);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(327, 22);
+            this.txtDescription.TabIndex = 117;
+            // 
+            // label76
+            // 
+            this.label76.AutoSize = true;
+            this.label76.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label76.Location = new System.Drawing.Point(139, 94);
+            this.label76.Name = "label76";
+            this.label76.Size = new System.Drawing.Size(15, 16);
+            this.label76.TabIndex = 116;
+            this.label76.Text = "£";
+            // 
+            // txtCharge
+            // 
+            this.txtCharge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCharge.Location = new System.Drawing.Point(165, 91);
+            this.txtCharge.Name = "txtCharge";
+            this.txtCharge.Size = new System.Drawing.Size(71, 22);
+            this.txtCharge.TabIndex = 115;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(139, 121);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(15, 16);
+            this.label14.TabIndex = 114;
+            this.label14.Text = "£";
+            // 
+            // txtRate
+            // 
+            this.txtRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRate.Location = new System.Drawing.Point(165, 118);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(71, 22);
+            this.txtRate.TabIndex = 113;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Description:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Rate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Charge:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 120;
+            this.label1.Text = "Teacher:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 121;
+            this.label2.Text = "School:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 16);
+            this.label6.TabIndex = 122;
+            this.label6.Text = "Day:";
+            // 
+            // lblTeacher
+            // 
+            this.lblTeacher.AutoSize = true;
+            this.lblTeacher.Location = new System.Drawing.Point(165, 13);
+            this.lblTeacher.Name = "lblTeacher";
+            this.lblTeacher.Size = new System.Drawing.Size(53, 16);
+            this.lblTeacher.TabIndex = 123;
+            this.lblTeacher.Text = "teacher";
+            // 
+            // lblSchool
+            // 
+            this.lblSchool.AutoSize = true;
+            this.lblSchool.Location = new System.Drawing.Point(165, 40);
+            this.lblSchool.Name = "lblSchool";
+            this.lblSchool.Size = new System.Drawing.Size(48, 16);
+            this.lblSchool.TabIndex = 124;
+            this.lblSchool.Text = "school";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(165, 67);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(31, 16);
+            this.lblDate.TabIndex = 125;
+            this.lblDate.Text = "day";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(379, 180);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(113, 27);
+            this.btnUpdate.TabIndex = 126;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // frmEditBooking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 225);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblSchool);
+            this.Controls.Add(this.lblTeacher);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label76);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCharge);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.label14);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmEditBooking";
+            this.Text = "Edit Booking";
+            this.Load += new System.EventHandler(this.frmEditBooking_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label76;
+        private System.Windows.Forms.TextBox txtCharge;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTeacher;
+        private System.Windows.Forms.Label lblSchool;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
---
+++ RedboxAddin/Presentation/frmEditBooking.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RedboxAddin.DL;
+
+namespace RedboxAddin.Presentation
+{
+    public partial class frmEditBooking : Form
+    {
+        long _bookingID;
+        string _teacher = null;
+        string _school = null;
+        string _date = null;
+
+
+        public frmEditBooking(long id, string teacher, string school, string date)
+        {
+            InitializeComponent();
+            _bookingID = id;
+            _teacher = teacher;
+            _school = school;
+            _date = date;
+        }
+
+        private void frmEditBooking_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Booking bb = LINQmanager.GetBookingbyID(_bookingID);
+                if (bb == null)
+                {
+                    MessageBox.Show("The booking could not be retrieved");
+                    this.Close();
+                }
+                else
+                {
+
+                    lblDate.Text = _date;
+                    lblSchool.Text = _school;
+                    lblTeacher.Text = _teacher;
+
+                    txtDescription.Text = bb.Description;
+                    txtCharge.Text = bb.Charge.ToString();
+                    txtRate.Text = bb.Rate.ToString();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int response = new DBManager().UpdateBooking(_bookingID, txtDescription.Text, txtRate.Text, txtCharge.Text);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+
+    }
+}
